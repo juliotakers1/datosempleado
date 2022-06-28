381529b99753f65a56621162152b211bdc1aeb1c
Marcos <marcos@ciclo.tech>
2022-06-28 07:07:42 -0600

    prueba

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-const User = require ('../models/User');
+//const User = require ('../models/User');
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
