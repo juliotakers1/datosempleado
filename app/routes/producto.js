0d74abe03be7d20a2e90cf98872cc44c3bfd2646
Marcos <marcos@ciclo.tech>
2022-06-30 07:16:09 -0600

    cambios auth

--- app/routes/producto.js
+++ app/routes/producto.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const {verificarAuth, verificarAdministrador} = require('../middlewares/autenticacion.js');
+
 const ProductoCtrl = require('../controllers/ProductoController');
 
 const Router = express.Router();
 
-Router.get('/',verificarAuth,ProductoCtrl.index) //api.com/Producto/
-        .post('/', verificarAuth,ProductoCtrl.create)
-        .get('/:key/:value', verificarAuth,ProductoCtrl.find, ProductoCtrl.show) 
-        .put('/:key/:value', verificarAuth,ProductoCtrl.find,ProductoCtrl.update)
-        .delete('/:key/:value', verificarAuth,ProductoCtrl.find,ProductoCtrl.remove);
+Router.get('/',ProductoCtrl.index) //api.com/Producto/
+        .post('/', ProductoCtrl.create)
+        .get('/:key/:value', ProductoCtrl.find, ProductoCtrl.show) 
+        .put('/:key/:value', ProductoCtrl.find,ProductoCtrl.update)
+        .delete('/:key/:value', ProductoCtrl.find,ProductoCtrl.remove);
 
 module.exports = Router; 
